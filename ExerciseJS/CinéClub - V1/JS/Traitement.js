73568a5db6c3429a1aaac9531921a182a3b04b02
RoyJe1733240 <royje1733240@etu.cegepjonquiere.ca>
2018-11-01 20:22:33 -0400

    CinéClub1

---
+++ ExerciseJS/CinéClub - V1/JS/Traitement.js
@@ -0,0 +1,23 @@
+function btnCalculer_onclick()
+{
+    var Representation, Age, Res, PrixBase,Rabais;
+    PrixBase = parseInt(document.getElementById("txtBasePrix").value);
+    if((Representation = document.getElementById("radSoir").checked = true) && (Age <= parseInt(document.getElementById("txtAge").value)))
+    {
+        Rabais = 0.4;
+    }
+    else if ((Representation = document.getElementById("radMidi").checked = true) && (Age <= parseInt(document.getElementById("txtAge").value)))
+    {
+        Rabais = 0.6;
+    }
+    else if ((Representation = document.getElementById("radSoir").checked = true ) && (Age > parseInt(document.getElementById("txtAge").value)))
+    {
+        Rabais = 0;
+    }
+    else
+    {
+        Rabais = 0.3;
+    }
+    Res = PrixBase * Rabais;
+    console.log("le prix total sera de " + Res)
+}
