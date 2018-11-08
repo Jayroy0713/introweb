f1cdba571a295940b2dd1536ad885231ae7ce320
RoyJe1733240 <royje1733240@etu.cegepjonquiere.ca>
2018-11-08 09:27:13 -0500

    Exercice Garderie animaux // Terminé

--- ExerciseJS/GarderieAnimaux/JS/traitement.js
+++ ExerciseJS/GarderieAnimaux/JS/traitement.js
@@ -1,26 +1,74 @@
 function btnCalculer_onclick()
 {
-    var tarif,animal,jours;
- 	if (Document.GetElementById(lstVeterinaire).value = Document.GetElementById(Audrey).value)
-    {
-        Document.GetElementById(tarif).value = 48.29;
-        Document.GetElementById(radChien).checked = true;
-        Document.GetElementById(chkServ).checked = true;
-        Document.GetElementById(txtNbreJours).value = 2;
+    var tarif,nomvet,nbrjours,taux,tot;
+    heures = parseInt(document.getElementById("txtNbrHrs").value);
+    nbrjours = parseInt(document.getElementById("txtNbreJours").value);
+    nomvet = document.getElementById("lstVeterinaire").value;
 
-        If (Document.GetElementByIdById(chkServ).checked = true)
+    if (nbrjours >=1 && nbrjours<5 )
+    {
+        rabais = 0
+    }
+    else if (nbrjours >=5 && nbrjours <10)
+    {
+        rabais = 0.05
+    }
+    else if (nbrjours >=10 && nbrjours <30)
+    {
+        rabais = 0.10
+    }
+    else if (nbrjours >= 30)
+    {
+        rabais = 0.15
+    }
+
+    if (document.getElementById("lstVeterinaire").value = document.getElementById("Audrey").value)
+    {
+        if (document.getElementById("radChien").checked = true)
+        {
+        tarif = 48.29;
+        document.getElementById("chkServ").checked = true;
+            nbrjours = 2;
+
+        }
+
+
+    }
+    else if (nomvet = document.getElementById("Max").value)
+    {
+        if ( document.getElementById("radChat").checked = true)
+        {
+        tarif = 77.87;
+        document.getElementById("chkServ").checked = false;
+            nbrjours = 4;
+        }
+        if (document.getElementById("chkServ").checked = true)
         {
             tarif = tarif + 5;
         }
 
     }
-    else if (Document.GetElementById(lstVeterinaire).value = Document.GetElementById(Max).value)
+    switch (nomvet)
     {
-        Document.GetElementById(tarif).value = 77.87;
-        Document.GetElementById(radChat).checked = true;
-        Document.GetElementById(chkServ).checked = false;
-        Document.GetElementById(txtNbreJours).value = 4;
-
+        case "Audrey Bouchard": taux = 25;
+            break;
+        case "Stéphane Tremblay": taux = 35;
+            break;
+        case "Maxime Simard": taux = 40;
+            break;
+        case "Mélissa Caron": taux = 45;
+            break;
     }
-    console.log("Le prix sera donc de "+tarif +" pour "+txtNbreJours +" jours.")
+ tarif = tarif + (taux * heures);
+
+
+    (document.getElementById("lblMessage").innerHTML = "Le prix sera de "+tarif +" pour "+nbrjours +" jours.");
+}
+function radChat_onclick()
+{
+   document.getElementById("lblimg").src =  "GarderieAnimaux/img/Chat.jpg"
+}
+function radChien_onclick()
+{
+    document.getElementById("lblimg").src =  "GarderieAnimaux/img/chien.jpg"
 }
