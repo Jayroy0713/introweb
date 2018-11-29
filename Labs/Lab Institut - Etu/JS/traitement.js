a512a012dcabb3d8626e544fd0480336b740aac4
RoyJe1733240 <royje1733240@etu.cegepjonquiere.ca>
2018-11-29 10:37:42 -0500

    Lab institut // Presque Finit

---
+++ Labs/Lab Institut - Etu/JS/traitement.js
@@ -0,0 +1,103 @@
+function frmMembre_onclick()
+{
+ var Valide = false;
+ if(valideChampsOblig()===true);
+    {
+        traiterInfos();
+    }
+}
+function traiterInfos()
+{
+   var nom,prenom,adresse,ville, codePostal,telephone,codePerm,age;
+    nom = (document.getElementById("txtNom").value);
+    prenom = (document.getElementById("txtPrenom").value);
+    adresse = (document.getElementById("txtAdresse").value);
+    ville = (document.getElementById("txtVille").value);
+    codePostal = (document.getElementById("txtCodePostal").value);
+    telephone = (document.getElementById("txtTel").value);
+    codePerm = (document.getElementById("txtCodePerm").value);
+    age = (document.getElementById("type").value);
+}
+function valideChampsOblig()
+{
+    var Valide = true, tabNomId = new Array("txtNom","txtPrenom","txtAdresse","txtVille","txtTel");
+    for (i=0;i<5;i++)
+    {
+        if (validExist(tabNomId[i])==false)
+        {
+            Valide = false
+        }
+    }
+    return Valide
+}
+function validExist(nomId)
+{
+    var Valide;
+    if(document.getElementById(nomId).value ==="")
+    {
+        Valide = false;
+        document.getElementById(nomId).style.borderColor = "red";
+        document.getElementById("lblMessageErreur").innerHTML ="Tous les champs possédant une étoiles sont obligatoires!!"
+    }
+    else
+    {
+        Valide = true;
+        document.getElementById(nomId).style.borderColor = "white";
+    }
+    return Valide
+}
+function ValideFormat(nomId)
+{
+    var Valide = true;
+    if (ValideChaine(document.getElementById("txtNom").value) === false);
+    {
+        Valide = false;
+        document.getElementById("txtNom").style.borderColor = "red";
+    }
+    if (ValideChaine(document.getElementById("txtPrenom").value) === false);
+    {
+        Valide = false;
+        document.getElementById("txtPrenom").style.borderColor = "red";
+    }
+    if (ValideChaine(document.getElementById("txtVille").value) === false);
+    {
+        Valide = false;
+        document.getElementById("txtVille").style.borderColor = "red";
+    }
+    if ((ValideCodePost(document.getElementById("txtCodePostal").value) === false)  && document.getElementById("txtCodePostal").value != false);
+    {
+        Valide = false;
+        document.getElementById("txtCodePostal").style.borderColor = "red";
+    }
+    if (ValideTel(document.getElementById("txtTel").value) === false);
+    {
+        Valide = false;
+        document.getElementById("txtTel").style.borderColor = "red";
+    }
+    if ((ValideCodePerm(document.getElementById("txtCodePerm").value) === false) && document.getElementById("txtCodePostal").value != false);
+    {
+        Valide = false;
+        document.getElementById("txtCodePerm").style.borderColor = "red";
+    }
+    if (ValideAdresse(document.getElementById("txtAdresse").value) === false);
+    {
+        Valide = false;
+        document.getElementById("txtAdresse").style.borderColor = "red";
+    }
+}
+function ValideChaine(Chaine)
+{
+    return /^[A-z]+\-?[A-z]+?$/.test(Chaine)
+}
+function ValideCodePost(Chaine)
+{
+    return /^[A-Z][0-9][A-Z] [0-9][A-z][0-9]|[a-z][0-9][a-z] [0-9][a-z][0-9]$/.test(Chaine)
+}
+function ValideTel(Chaine)
+{
+    return /^[0-9]{3}\-[0-9]{3}\-[0-9]{4}|[(][0-9]{3}[)] [0-9]{3}\-[0-9]{4}$/.test(Chaine)
+}
+function ValideCodePerm(Chaine)
+{
+    return /^[A-Z]{4}[0-9]{8}|^[[a-z]{4}[0-9]{8}$/.test(Chaine)
+}
